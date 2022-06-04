72dac4907a1655c11c93e80e6fc17f27f1752a0f
Ruan Beukes <ruan.coder@gmail.com>
2022-06-04 14:22:15 +0800

    remove commented code which came with VS template

--- Source/Moq.QuickMock/MoqQuickMockCodeRefactoringProvider.cs
+++ Source/Moq.QuickMock/MoqQuickMockCodeRefactoringProvider.cs
@@ -207,24 +207,5 @@ namespace CodeRefactoring1
 
             // return originalSolution;
         }
-        //private async Task<Solution> ReverseTypeNameAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
-        //{
-        //    // Produce a reversed version of the type declaration's identifier token.
-
-        //    var identifierToken = typeDecl.Identifier;
-        //    var newName = new string(identifierToken.Text.ToCharArray().Reverse().ToArray());
-
-        //    // Get the symbol representing the type to be renamed.
-        //    var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
-        //    var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
-
-        //    // Produce a new solution that has all references to that type renamed, including the declaration.
-        //    var originalSolution = document.Project.Solution;
-        //    var optionSet = originalSolution.Workspace.Options;
-        //    var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
-
-        //    // Return the new solution with the now-uppercase type name.
-        //    return newSolution;
-        //}
     }
 }
