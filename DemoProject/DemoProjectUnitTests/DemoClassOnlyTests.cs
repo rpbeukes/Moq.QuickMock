57016c8de70ed72c019fe791cea015c9290f0e97
Ruan Beukes <ruan.coder@gmail.com>
2022-06-26 16:03:05 +0800

    update demo proj

--- DemoProject/DemoProjectUnitTests/DemoClassOnlyTests.cs
+++ DemoProject/DemoProjectUnitTests/DemoClassOnlyTests.cs
@@ -32,17 +32,25 @@ namespace DemoProject.Tests
                                                    Mock.Of<Func<IValidator<InvoiceDetailsInput>>>());
         }
 
+        // this will not be removed
+        Mock<Func<SomeCommand>> _cmdFactoryMock = new Mock<Func<SomeCommand>>();
+
         [TestMethod()]
         public void DemoClassOnlyTest_MockObject_to_MockOfT_local_variable()
         {
-            var currentUserMock = new Mock<ICurrentUser>();
+            // this will not be removed
+            Mock<ICurrentUser> currentUserMock;
+            currentUserMock = new Mock<ICurrentUser>();
+
+            // this will be removed
             var validatorFactoryMock = new Mock<Func<IValidator<InvoiceDetailsInput>>>();
+            
             var systemUnderTest = new DemoClassOnly(Mock.Of<ILogger<DemoClassOnly>>(),
                                                    It.IsAny<string>(),
                                                    It.IsAny<int>(),
                                                    It.IsAny<int?>(),
                                                    currentUserMock.Object,
-                                                   Mock.Of<Func<SomeCommand>>(),
+                                                   _cmdFactoryMock.Object,
                                                    validatorFactoryMock.Object);
         }
     }
