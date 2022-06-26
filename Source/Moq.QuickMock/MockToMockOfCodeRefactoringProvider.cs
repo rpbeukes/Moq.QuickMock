97f172706d74c5706d11bbc4cb63034d286621c8
Ruan Beukes <ruan.coder@gmail.com>
2022-06-26 15:07:08 +0800

    works

--- Source/Moq.QuickMock/MockToMockOfCodeRefactoringProvider.cs
+++ Source/Moq.QuickMock/MockToMockOfCodeRefactoringProvider.cs
@@ -4,6 +4,8 @@ using Microsoft.CodeAnalysis.CodeRefactorings;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Simplification;
 using System;
 using System.Composition;
 using System.Linq;
@@ -36,13 +38,57 @@ namespace Moq.QuickMock
 
             if (varType is null) return;
 
-            isActionAvailable = varType.OriginalDefinition.ToString() == "Moq.Mock<T>";
+            isActionAvailable = varType.OriginalDefinition.ToString().Equals("Moq.Mock<T>", StringComparison.OrdinalIgnoreCase);
+
+            if (!isActionAvailable) return;
 
             if (isActionAvailable)
             {
                 var action = CodeAction.Create("mock.Object to Mock.Of<T> (Moq)", async (c) =>
                 {
+                    var localVarsInMethod = identifierName.Ancestors()
+                                                          .OfType<MethodDeclarationSyntax>()
+                                                          .FirstOrDefault()
+                                                          ?.DescendantNodes()
+                                                          .OfType<LocalDeclarationStatementSyntax>();
+
                     var editor = await DocumentEditor.CreateAsync(document);
+
+                    var objectCreationExpression = node.Ancestors().OfType<ObjectCreationExpressionSyntax>().First();
+                    var argumentList = node.Ancestors().OfType<ArgumentListSyntax>().FirstOrDefault();
+                    var typeInfoOfClass = semanticModel.GetTypeInfo(objectCreationExpression);
+                    var classDefinition = typeInfoOfClass.ConvertedType as INamedTypeSymbol;
+                    var theCorrectConstructor = classDefinition.Constructors.First(x => x.Parameters.Length == argumentList.ChildNodes().Count());
+                    var currentArg = node.Ancestors().OfType<ArgumentSyntax>().FirstOrDefault();
+                    var argPosition = argumentList.Arguments.IndexOf(a => a.Span == currentArg.Span);
+                    var paramSymbol = theCorrectConstructor.Parameters[argPosition];
+                    var paramSymbolWithoutFullyQualifiedNames = paramSymbol.Type.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat);
+                    var newExpression = SyntaxFactory.ParseExpression($"Mock.Of<{paramSymbolWithoutFullyQualifiedNames}>()")
+                                            .WithAdditionalAnnotations(Formatter.Annotation, Simplifier.Annotation)
+                                            .WithLeadingTrivia(currentArg.GetLeadingTrivia());
+
+                    var newArg = SyntaxFactory.Argument(newExpression);
+
+                    editor.ReplaceNode(currentArg, newArg);
+
+                    var localVarMatchingArgumentName = localVarsInMethod?.FirstOrDefault(x => x.Declaration.Variables.FirstOrDefault()?.Identifier.ToString().Equals(identifierName.Identifier.ToString(), StringComparison.OrdinalIgnoreCase)
+                                                                                        ?? false);
+                    if (localVarMatchingArgumentName != null)
+                    {
+                        var thisIsANewMockInstance = localVarMatchingArgumentName
+                                                         .DescendantNodes()
+                                                         .OfType<ObjectCreationExpressionSyntax>()
+                                                         .FirstOrDefault(x => x.Parent.ToString().Contains($"= new Mock<{paramSymbolWithoutFullyQualifiedNames}>"))
+                                                         ;
+
+                        // will only remove local declared variables
+                        // if a member variable, don't touch
+                        if (thisIsANewMockInstance != null)
+                        {
+                            // found the variable declaration, remove it.
+                            editor.RemoveNode(localVarMatchingArgumentName);
+                        }
+                    }
 
                     var changedDoc = editor.GetChangedDocument();
                     return changedDoc;
