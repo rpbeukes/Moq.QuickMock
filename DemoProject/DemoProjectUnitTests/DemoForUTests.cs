d338adc482e1d4ad5dd5e55656bbce3a94c701cc
Ruan Beukes <ruan.coder@gmail.com>
2025-03-16 15:28:35 +0800

    add unit test file

---
+++ DemoProject/DemoProjectUnitTests/DemoForUTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DemoProject.Tests
+{
+    /// <summary>
+    /// Demo for uni tests
+    /// </summary>
+    public class DemoForUTests
+    {
+        public DemoForUTests(string stringValue, int intValue)
+        {
+
+        }
+    }
+
+    public class DemoForUTTests
+    {
+        public void DemoForUT_Quick_mock()
+        {
+
+            var systemUnderTest = new DemoForUTests();
+
+        }
+    }
+}
