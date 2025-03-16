d9be5f60d354ae1e3a3ded2c86cdff82264d09d6
Ruan Beukes <ruan.coder@gmail.com>
2025-03-16 15:27:58 +0800

    attempt to add unit tests (still Work In Progress)

---
+++ Source/Moq.QuickMock.Vsix.Tests/MoqQuickMockCodeRefactoringProviderTests.cs
@@ -0,0 +1,96 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VerifyCS = Moq.QuickMock.Test.CSharpCodeRefactoringVerifier<
+    Moq.QuickMock.MoqQuickMockCodeRefactoringProvider>;
+
+namespace Moq.QuickMock.Tests;
+
+[TestClass]
+public class MoqQuickMockCodeRefactoringProviderTests
+{
+
+    [TestMethod]
+    public async Task TriggerBothDiagnosticsAndCodeFix()
+    {
+        var test = @"
+using System;
+
+namespace DemoProject.Tests
+{
+    /// <summary>
+    /// Demo for uni tests
+    /// </summary>
+    public class DemoForUTests
+    {
+        public DemoForUTests(string stringValue, int intValue)
+        {
+
+        }
+    }
+
+    public class DemoForUTTests
+    {
+        public void DemoForUT_Quick_mock()
+        {
+            var systemUnderTest = new DemoForUTests({|#0:|});
+        }
+    }
+}
+";
+
+        var fixtest = @"
+using System;
+
+namespace DemoProject.Tests
+{
+    /// <summary>
+    /// Demo for uni tests
+    /// </summary>
+    public class DemoForUTests
+    {
+        public DemoForUTests(string stringValue, int intValue)
+        {
+
+        }
+    }
+
+    public class DemoForUTTests
+    {
+        public void DemoForUT_Quick_mock()
+        {
+            var systemUnderTest = new DemoForUTests(It.IsAny<string>(), It.IsAny<int>());
+        }
+    }
+}
+";
+
+        //var systemUnderTest = new DemoForUTests(It.IsAny<string>(), It.IsAny<int>());
+        //var systemUnderTest = new DemoForUTests(It.IsAny<string stringValue>(), It.IsAny<int intValue>());
+
+        var expectedDiagnostic = DiagnosticResult
+                                    .CompilerError("CS7036")
+                                    .WithSpan(21, 39, 21, 52)
+                                    .WithArguments("stringValue", "DemoProject.Tests.DemoForUTests.DemoForUTests(string, int)")
+                                    ;
+
+        var expectedRefactoring = DiagnosticResult
+                                    .CompilerError("Refactoring")
+                                    .WithSpan(21, 53, 21, 53)
+                                    ;
+
+
+        //var rrr = new DiagnosticResultSpan
+        //{
+        //    
+        //}
+        await VerifyCS.VerifyRefactoringAsync(test,
+                                              fixtest,
+                                              [expectedDiagnostic, expectedRefactoring],
+                                              actionTitle: "Quick mock ctor (Moq)",
+                                              testBehaviors: TestBehaviors.SkipGeneratedCodeCheck);
+    }
+}
---
+++ Source/Moq.QuickMock.Vsix.Tests/Verifiers/CSharpCodeRefactoringVerifier`1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
+
+namespace Moq.QuickMock.Test
+{
+    public static partial class CSharpCodeRefactoringVerifier<TCodeRefactoring>
+        where TCodeRefactoring : CodeRefactoringProvider, new()
+    {
+        /// <inheritdoc cref="CodeRefactoringVerifier{TCodeRefactoring, TTest, TVerifier}.VerifyRefactoringAsync(string, string)"/>
+        public static async Task VerifyRefactoringAsync(string source, string fixedSource)
+        {
+            await VerifyRefactoringAsync(source, DiagnosticResult.EmptyDiagnosticResults, fixedSource);
+        }
+
+        /// <inheritdoc cref="CodeRefactoringVerifier{TCodeRefactoring, TTest, TVerifier}.VerifyRefactoringAsync(string, DiagnosticResult, string)"/>
+        public static async Task VerifyRefactoringAsync(string source, DiagnosticResult expected, string fixedSource)
+        {
+            await VerifyRefactoringAsync(source, new[] { expected }, fixedSource);
+        }
+
+        /// <inheritdoc cref="CodeRefactoringVerifier{TCodeRefactoring, TTest, TVerifier}.VerifyRefactoringAsync(string, DiagnosticResult[], string)"/>
+        public static async Task VerifyRefactoringAsync(string source, DiagnosticResult[] expected, string fixedSource)
+        {
+            var test = new Test
+            {
+                TestCode = source,
+                FixedCode = fixedSource,
+            };
+
+            test.ExpectedDiagnostics.AddRange(expected);
+            await test.RunAsync(CancellationToken.None);
+        }
+
+        public static async Task VerifyRefactoringAsync(string source,
+                                                        string fixedSource,
+                                                        DiagnosticResult[] expected = null,
+                                                        string actionTitle = null,
+                                                        TestBehaviors testBehaviors = TestBehaviors.None)
+        {
+            var test = new Test
+            {
+                TestCode = source,
+                FixedCode = fixedSource,
+                TestBehaviors = testBehaviors,
+                //CompilerDiagnostics = CompilerDiagnostics.Errors
+            };
+
+            //test.TestCode = source;
+            //test.FixedCode = fixedSource;
+
+            if (expected != null && expected.Any())
+            {
+                test.ExpectedDiagnostics.AddRange(expected);
+            }
+
+            if (actionTitle != null)
+            {
+                test.CodeActionEquivalenceKey = actionTitle;
+            }
+
+            var srce = test.TestState.Sources[0];
+            srce.filename = srce.filename.Replace("Tests0", "Tests");
+            test.TestState.Sources[0] = srce;
+
+            srce = test.FixedState.Sources[0];
+            srce.filename = srce.filename.Replace("Tests0", "Tests");
+            test.FixedState.Sources[0] = srce;
+
+            await test.RunAsync();
+
+
+        }
+    }
+}
